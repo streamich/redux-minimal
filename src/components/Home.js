ecd6433909fd9c7d7b96fa555b18e6e3c49d49de
streamich <streamich@gmail.com>
2018-03-30 15:55:18 +0100

    chore: add nano-css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
+import {rule} from '../nano';
+
+const className = rule({
+  pad: '20px'
+});
 
 // Home page component
 export default class Home extends React.Component {
   // render
   render() {
     return (
-      <div className="page-home">
+      <div className={className}>
         <h4>Hello world!</h4>
       </div>
     );
---
+++ src/nano.js
@@ -0,0 +1,18 @@
+import {create} from 'nano-css';
+import {addon as addonRule} from 'nano-css/addon/rule';
+import {addon as addonCache} from 'nano-css/addon/cache';
+import {addon as addonJsx} from 'nano-css/addon/jsx';
+import {addon as addonAtoms} from 'nano-css/addon/atoms';
+
+const nano = create();
+
+addonRule(nano);
+addonAtoms(nano);
+
+const {rule, jsx} = nano;
+
+export {
+    nano,
+    rule,
+    jsx,
+};

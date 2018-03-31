dcc5fc3b7e54cf50cfa6a83b1a41bf0e33a6a07d
streamich <streamich@gmail.com>
2018-04-01 00:54:21 +0100

    feat: debounce history by 500ms

--- src/sagas/history.js
+++ src/sagas/history.js
@@ -13,21 +13,33 @@ import {
 import { getIcon } from '../selectors';
 
 const pushIntoHistory = function* ({uuid}) {
+  yield call(delay, 500);
+
   const icon = yield select(getIcon, uuid);
 
   yield put(historyPush(icon));
 };
 
 const watchHistoriEvents = function* () {
-  yield takeEvery([
-    ICONS_CREATE,
-    ICONS_RENAME,
-    ICONS_TAG_ADD,
-    ICONS_TAG_REMOVE,
-    ICONS_SET_COLOR,
-    ICONS_SELECT_COLOR,
-    ICONS_PUT_COLOR,
-  ], pushIntoHistory);
+  let task;
+
+  while (true) {
+    const action = yield take([
+      ICONS_CREATE,
+      ICONS_RENAME,
+      ICONS_TAG_ADD,
+      ICONS_TAG_REMOVE,
+      ICONS_SET_COLOR,
+      ICONS_SELECT_COLOR,
+      ICONS_PUT_COLOR,
+    ]);
+
+    if (task) {
+      yield cancel(task);
+    }
+
+    task = yield fork(pushIntoHistory, action);
+  }
 };
 
 export default function * () {
